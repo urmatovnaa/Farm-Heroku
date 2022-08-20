144c9f85aa0d6340c444f7c377771715a39edfe0
Altynai <urmatovnaa05@gmail.com>
2022-08-20 16:52:18 +0600

    add static root

--- farm/settings.py
+++ farm/settings.py
@@ -133,6 +133,8 @@ MODELTRANSLATION_PREPOPULATE_LANGUAGE = 'ru'
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
