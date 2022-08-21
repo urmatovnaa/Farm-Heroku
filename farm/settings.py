66c2e92d7ab5c3b9268c946ab7945c41f86b172c
Altynai <urmatovnaa05@gmail.com>
2022-08-21 19:49:29 +0600

    add media

--- farm/settings.py
+++ farm/settings.py
@@ -135,7 +135,7 @@ MODELTRANSLATION_PREPOPULATE_LANGUAGE = 'ru'
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -151,7 +151,7 @@ EST_FRAMEWORK = {
 }
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # CORS
 
