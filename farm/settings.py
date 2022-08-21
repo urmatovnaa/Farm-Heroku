9ce4cc268af5a6c92032f0c105224666e10129d7
Altynai <urmatovnaa05@gmail.com>
2022-08-21 19:23:59 +0600

    add django-cors-for js

--- farm/settings.py
+++ farm/settings.py
@@ -151,6 +151,33 @@ EST_FRAMEWORK = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# CORS
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # Configure Django App for Heroku.
 import django_on_heroku
 
